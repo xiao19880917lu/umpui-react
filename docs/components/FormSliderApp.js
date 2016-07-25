27a3958a599993125a5cba53e41df3aabd1f6dc6
renxintao <renxintao@baidu.com>
2016-07-25 13:14:19 +0800

    优化form组件及formSlide组件及开发文档修改
    Change-Id: If5259dce46cddf42ee4beafce4391792bdf8e5c3

--- docs/components/FormSliderApp.js
+++ docs/components/FormSliderApp.js
@@ -11,17 +11,42 @@ export default class FormSliderApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            style: {display: 'block'}
         };
     }
-    handleFormSlider(event) {
-        event.stopPropagation();
+
+    /**
+     * 表单提交按钮点击触发函数
+     * @param {Array<Object>} formData 表单参数
+     * @param {Array<Object>} form 该form表单配置
+     */
+    handleFormSliderSubmit(formData, form) {
+        this.setState({style: {display: 'none'}});
+        console.log(formData);
+        console.log(form);
+    }
+
+    /**
+     * 表单取消按钮点击触发函数
+     */
+    onCancel()  {
+        this.setState(
+            {style: {display: 'none'},
+            sliderConfig: PageData.sliderConfig});
+    }
+    // list列表删除按钮回调函数
+    onDeleteDashboard(dashboardName) {
+        console.log(dashboardName);
     }
     render() {
         const mdText = require('text!../mdFile/formSlider.md');
         return (<div className="umpui-component">
             <h3 className="umpui-layer umpui-title">级联分步骤的表单组件</h3>
+            <div style={this.state.style}>
             <FormSlider formConfig={PageData.sliderConfig} fromData={PageData.formData}
-                submit={this.handleFormSlider} />
+            submit={this.handleFormSliderSubmit.bind(this)} cancel={this.onCancel.bind(this)}
+            onDeleted={this.onDeleteDashboard.bind(this)}/>
+            </div>
             <div className="umpui-layer umpui-block">
                 <MarkdownElement text={mdText}/>
             </div>
--- docs/mdFile/form.md
+++ docs/mdFile/form.md
@@ -3,6 +3,8 @@
 
 ### 参数说明:  
 
+    title: form标题
+    removeIcon：是否配置右上角X号
     config: form表单配置
 ### 使用方式:  
 
@@ -12,6 +14,7 @@ import ReactDOM from 'react-dom';
 import {ReactForm, ReactTransverForm} from 'umpui-react';
 let formConfig = {
     title: '执行工具接口说明', // 表单的title
+    removeIcon: true, //是否配置右上角X号
     formConfig: [
         {
             type: 'select', // 表单的类型
@@ -36,11 +39,23 @@ let formConfig = {
             fill: true,
             defaultValue: 'Test Test',
             placeholder: '工具执行方法或url, 视api类型而定, http形式即为合法url, rms开放平台或rpc即为方法名称(方法名称不需要携带括号)'
-        }
+        }, {
+            type: 'list',
+            ref: 'dashboards',
+            list: ['dashboard1','dashboard2','dashboard3'],
+            trashIcon:true, //是否配置list表删除操作
+        }]
     ]
 };
+// 点击右上角叉号回调函数
+onCancel()  {
+}
+// list单中删除图表回调函数
+onDelete(item) {
+console.log(item);
+}
 // 纵向form表单
-<ReactForm ref="apiForm" config={formConfig}/>
+<ReactForm ref="apiForm" config={formConfig} onCancel={this.onCancel.bind(this) onDelete={this.onDelete.bind(this)}/>
 // 横向Form表单
 <ReactTransverForm ref="apiForm" config={formConfig}/>
 // 获取表单值的方式,返回对象{k:v,k1:v1} k为formConfig中的ref
