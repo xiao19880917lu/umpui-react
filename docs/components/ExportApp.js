2790d870bd5679672c407e53453efe26bf96aa56
liuzechun <liuzechun@baidu.com>
2016-08-12 14:20:51 +0800

    数据导出模块优化: 导出按钮自己定制，导出时的提示信息可以定
    Change-Id: Ic093431964e3ab6c8f2c9b555f58bf7e0d99cab4

--- docs/components/ExportApp.js
+++ docs/components/ExportApp.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MarkdownElement from '../../lib/MarkdownElement.js';
 import Export from '../../lib/src/Export.js';
+import {Button} from 'antd';
 export default class ExprotApp extends React.Component {
     constructor(props) {
         super(props);
@@ -13,7 +14,7 @@ export default class ExprotApp extends React.Component {
     render() {
         const mdText = require('text!../mdFile/export.md');
         // 以下均为模拟数据，在实际应用中可根据情况获取
-        let config = {
+        let config1 = {
             // 导出数据接口的url（一般可直接使用分页接口）
             url: 'php/download.php',
             search: {
@@ -29,14 +30,35 @@ export default class ExprotApp extends React.Component {
             // 表格中要显示的字段，以及字段对应的表头
             headers: {'id': 'ID', 'hostname': '主机名', 'sn': 'SN', 'status': '状态', 'model_id': '型号', 'rack': '机架位'},
             // 当前表格所有数据的总条数
-            total: 1320,
-            // 其他用户自定义传给后台的参数
-            otherparms: {}
+            total: 720
+        };
+        let config2 = {
+            url: 'php/download.php',
+            search: {
+                keys: 'id,hostname,sn,status,model_id,rack,container_id,rms_product_id',
+                conditions: {
+                    'container_id': 484,
+                    'type': 'server'
+                }
+            },
+            headers: {'id': 'ID', 'hostname': '主机名', 'sn': 'SN', 'status': '状态', 'model_id': '型号', 'rack': '机架位'},
+            total: 720,
+            otherparms: {},
+            message: {
+                page1: ['请注意，程序会自动根据分页大小依次向服务端请求数据，全部请求完毕后生成Excel下载。不要将分页大小设置的过大，以免服务器端查询数据超时。'],
+                page2: ['如果下载的文件用Excel打开提示文件格式与扩展名不一致，请选择“是”，直接用Excel打开即可。", "为防止常规单元格式下excel的自动转化，所有字段均转化为文本！']
+            }
         };
         return (
             <div className="umpui-component">
                 <h3 className="umpui-layer umpui-title">数据导出</h3>
-                <Export config={config} />
+                <Export config={config1}>
+                    <Button key='1' type = "primary">默认导出</Button>
+                </Export>
+                &nbsp;&nbsp;&nbsp;
+                <Export config={config2}>
+                    <Button key='2' type = "primary">数据导出</Button>
+                </Export>
                 <div className="umpui-layer umpui-block">
                     <MarkdownElement text={mdText}/>
                 </div>
--- docs/mdFile/export.md
+++ docs/mdFile/export.md
@@ -1,4 +1,5 @@
 ### 参数说明
+    必要参数：
     url: String 导出数据接口的url（一般可直接使用分页接口）
     search: {} 存放搜索内容的对象 
     {   
@@ -7,7 +8,14 @@
     }  
     headers: {} 表格中要显示的字段，以及字段对应的表头（key=>value形式）
     total: Number 当前表格所有数据的总条数   
+    
+    可选参数：
     otherparms: {} 其它用户自定义传给后台的参数
+    message: {} 自定义提示信息
+    {
+        page1: []   page1为第一页即导出之前设置页面的提示信息
+        page2: []   page2为导出过程中的提示信息，可以传入多条
+    }
 ### 源代码  
  
 ```
@@ -15,7 +23,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Export from 'umpui-react';
 
-let config = {
+let config2 = {
     url: "php/download.php",   
     search: {
         keys: 'id,hostname,sn,status,model_id,rack,container_id,rms_product_id', 
@@ -26,13 +34,17 @@ let config = {
         }
     },
     headers: {'id': 'ID', 'hostname': '主机名', 'sn': 'SN', 'status': '状态', 'model_id': '型号', 'rack': '机架位'},
-    total: 1320,
+    total: 720,
     otherparms: {
         isExport: true,
-        pageType: 'server'
+    },
+    message: {
+        page1: ["请注意，程序会自动根据分页大小依次向服务端请求数据，全部请求完毕后生成Excel下载。不要将分页大小设置的过大，以免查询数据超时。"],
+        page2: ["如果下载的文件用Excel打开提示文件格式与扩展名不一致，请选 择“是”，直接用Excel打开即可。","为防止常规单元格式下excel的自动转化，所有字段均转化为文本！"]
     }
 }
     
-<Export config={config} />
-
+<Export config={config2}>
+    <Button type = "primary">数据导出</Button>
+</Export>
 ```
