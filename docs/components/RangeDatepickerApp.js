1254f044ea453d61eea00eb18ca7762a6bbee8a6
liuzechun <liuzechun@baidu.com>
2016-09-28 13:18:34 +0800

    升级RangeDatepicker组件，添加get/set方法，修复getMonth返回值-1的bug
    Change-Id: I3dfd77f0296b70031666683467023913cb489ff1

--- docs/components/RangeDatepickerApp.js
+++ docs/components/RangeDatepickerApp.js
@@ -4,20 +4,34 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from 'antd';
 import MarkdownElement from '../../lib/MarkdownElement.js';
 import RangeDatepicker from '../../lib/src/RangeDatepicker.js';
 export default class RangeDatepickerApp extends React.Component {
     constructor(props) {
         super(props);
     }
-    onChange(start, end) {
+    onChange({start, end}) {
         console.log(start, end);
+    }
+    getValue() {
+        let result = this.refs.datepicker.getValue();
+        console.log(result);
+    }
+    setValue() {
+        let start = '2016-08-13 13:09:58';
+        let end = '2016-09-28 13:00:58';
+        // start = new Date(start);
+        // end = new Date(end);
+        // this.refs.datepicker.setValue(start);
+        // this.refs.datepicker.setValue(null, end);
+        this.refs.datepicker.setValue(start, end);
     }
     render() {
         const mdText = require('text!../mdFile/rangeDatepicker.md');
         let config = {
-            showMenu: true
-            // ,defaultValue: {
+            showMenu: true,
+            // defaultValue: {
             //     start: '2016-08-13 13:09:58',
             //     end: '2016-08-16 19:09:58'
             // }
@@ -25,7 +39,12 @@ export default class RangeDatepickerApp extends React.Component {
         return (
             <div className="umpui-component">
                 <h3 className="umpui-layer umpui-title">日期/时间范围选择</h3>
-                <RangeDatepicker config={config} onChange={this.onChange.bind(this)}/>
+                <RangeDatepicker ref="datepicker" config={config} onChange={this.onChange.bind(this)}/>
+                <div>
+                    <Button type="primary" onClick={this.getValue.bind(this)}>获取数据</Button>
+                    &nbsp;&nbsp;&nbsp;
+                    <Button type="primary" onClick={this.setValue.bind(this)}>设置自定义日期</Button>
+                </div>
                 <div className="umpui-layer umpui-block">
                     <MarkdownElement text={mdText}/>
                 </div>
--- docs/mdFile/rangeDatepicker.md
+++ docs/mdFile/rangeDatepicker.md
@@ -8,6 +8,14 @@
 
     事件：
     onChange: 当输入框中内容变更时触发
+        接收参数： {start, end} 为一个对象，同getValue
+
+    对外接口：
+    getValue: 获取数据
+        return: {start, end} 返回值为一个对象，包括两个值，start/end
+    setValue: 设置自定义日期
+        params:(start[, end]) 参数为一个或两个值，第一个为起始时间，第二个为结束时间，可以是字符串，也可以是Date对象
+        return null;
 ### 源代码  
  
 ```
@@ -19,8 +27,21 @@ export default class RangeDatepickerApp extends React.Component {
     constructor(props) {
         super(props);
     }
-    onChange(start, end) {
+    onChange({start, end}) {
         console.log(start, end);
+    }
+    getValue() {
+        let result = this.refs.datepicker.getValue();
+        console.log(result);
+    }
+    setValue() {
+        let start = '2016-08-13 13:09:58';
+        let end = '2016-09-28 13:00:58';
+        // start = new Date(start);
+        // end = new Date(end);
+        // this.refs.datepicker.setValue(start);
+        // this.refs.datepicker.setValue(null, end);
+        this.refs.datepicker.setValue(start, end);
     }
     render() {
         let config = {
@@ -32,6 +53,8 @@ export default class RangeDatepickerApp extends React.Component {
         }
         return (
             <RangeDatepicker config={config} onChange={this.onChange.bind(this)}/>
+            <Button type="primary" onClick={this.getValue.bind(this)}>获取数据</Button>
+            <Button type="primary" onClick={this.setValue.bind(this)}>设置自定义日期</Button>
         );
     }
 }
